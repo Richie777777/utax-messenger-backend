eb511d8d7ca44f03aeae62ffc2a416afdc15ca59
Richie777777 <khurshidkhuja798@gmail.com>
2025-01-14 22:08:04 +0500

    change backend api

--- axios.js
+++ axios.js
@@ -2,7 +2,9 @@ import axios from 'axios'
 
 const token = '7917403316:AAGvI4WMUMQB3CzASZX0z6kd9TVyhn4HlaY'
 
-const BASE_URL = 'https://utax-messenger-backend.onrender.com'
+
+
+const BASE_URL = 'http://37.60.243.79:3000'
 const API_URL = `https://api.telegram.org/bot${token}`
 
 export const api = axios.create({
